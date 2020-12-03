bb983af1efcd055e742925243136c38db797fbcc
Jessica Blankemeier <jessicablankemeier@gmail.com>
2020-12-03 09:34:46 -0800

    Added three questions to questions.js

--- assets/questions.js
+++ assets/questions.js
@@ -1,16 +1,16 @@
 const questions = [
-    { question: `Strings in Javascript must be enclosed in what type of punctuation?`,
-      choices: [`quotes or backticks`, `brackets`, `curly braces`, `parenthesis`],
-      answer: `quotes or backticks`
+    { question: "Strings in Javascript are enclosed in what type of punctuation?",
+      choices: ["single or double quotes", "brackets", "curly braces", "parenthesis"],
+      answer: "single or double quotes"
     },
     {
-      question: `What will this for loop print to the console: for(let 1=0; i < 3; i++){console.log("Bumfuzzle")}`,
-      choices: [`Nothing`, `"Bumfuzzle" Once`, `"Bumfuzzle" Three Times`, `Infinitity "Bumfuzzle"`],
-      answer: `"Bumfuzzle" Three Times`
+      question: "What will this for loop print to the console: for(let 1=0; i < 3; i++){console.log('Bumfuzzle')}",
+      choices: ["Nothing", "'Bumfuzzle' Once", "'Bumfuzzle' Three Times", "Infinitity 'Bumfuzzle'"],
+      answer: "'Bumfuzzle' Three Times"
     },
     {
-        question: `What will this for loop print to the console: for(let 1=0; i < 3; i++){console.log("Bumfuzzle")}`,
-        choices: [`Nothing`, `"Bumfuzzle" Once`, `"Bumfuzzle" Three Times`, `Infinitity "Bumfuzzle"`],
-        answer: `"Bumfuzzle" Three Times`
+        question: "How could you assign a constant number value 600 as a variable using Javascript?",
+        choices: ["console.log(600)", "var value", "let value = 500 - 100", "const value = 600"],
+        answer: "const value = 600"
       }
 ]
