5f25dad5dcb8f7278d40454884979e28bd66aa10
Jessica Blankemeier <jessicablankemeier@gmail.com>
2020-12-02 21:56:18 -0800

    Added materialize and scss styling to home page, started on javascript questions

---
+++ assets/questions.js
@@ -0,0 +1,16 @@
+const questions = [
+    { question: `Strings in Javascript must be enclosed in what type of punctuation?`,
+      choices: [`quotes or backticks`, `brackets`, `curly braces`, `parenthesis`],
+      answer: `quotes or backticks`
+    },
+    {
+      question: `What will this for loop print to the console: for(let 1=0; i < 3; i++){console.log("Bumfuzzle")}`,
+      choices: [`Nothing`, `"Bumfuzzle" Once`, `"Bumfuzzle" Three Times`, `Infinitity "Bumfuzzle"`],
+      answer: `"Bumfuzzle" Three Times`
+    },
+    {
+        question: `What will this for loop print to the console: for(let 1=0; i < 3; i++){console.log("Bumfuzzle")}`,
+        choices: [`Nothing`, `"Bumfuzzle" Once`, `"Bumfuzzle" Three Times`, `Infinitity "Bumfuzzle"`],
+        answer: `"Bumfuzzle" Three Times`
+      }
+]
