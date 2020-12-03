c32ccb6db830480c951b97574bf7b38f071dd4bf
Jessica Blankemeier <jessicablankemeier@gmail.com>
2020-12-03 09:45:50 -0800

    Added 5 total questions

--- assets/questions.js
+++ assets/questions.js
@@ -1,16 +1,56 @@
 const questions = [
-    { question: "Strings in Javascript are enclosed in what type of punctuation?",
-      choices: ["single or double quotes", "brackets", "curly braces", "parenthesis"],
-      answer: "single or double quotes"
-    },
-    {
-      question: "What will this for loop print to the console: for(let 1=0; i < 3; i++){console.log('Bumfuzzle')}",
-      choices: ["Nothing", "'Bumfuzzle' Once", "'Bumfuzzle' Three Times", "Infinitity 'Bumfuzzle'"],
-      answer: "'Bumfuzzle' Three Times"
-    },
-    {
-        question: "How could you assign a constant number value 600 as a variable using Javascript?",
-        choices: ["console.log(600)", "var value", "let value = 500 - 100", "const value = 600"],
-        answer: "const value = 600"
-      }
-]
+  {
+    question: "Strings in Javascript are enclosed in what type of punctuation?",
+    choices: [
+      "single or double quotes",
+      "brackets",
+      "curly braces",
+      "parenthesis",
+    ],
+    answer: "single or double quotes",
+  },
+  {
+    question:
+      "What will this for loop print to the console: for(let 1=0; i < 3; i++){console.log('Bumfuzzle')}",
+    choices: [
+      "Nothing",
+      "'Bumfuzzle' Once",
+      "'Bumfuzzle' Three Times",
+      "Infinitity 'Bumfuzzle'",
+    ],
+    answer: "'Bumfuzzle' Three Times",
+  },
+  {
+    question:
+      "How could you assign a constant number value 600 as a variable using JavaScript?",
+    choices: [
+      "console.log(600)",
+      "var value",
+      "let value = 500 - 100",
+      "const value = 600",
+    ],
+    answer: "const value = 600",
+  },
+  {
+    question:
+      "What is an example of the syntax for a single line comment in JavaScript",
+    choices: [
+      "console.log(This is a comment)",
+      "// This is a comment",
+      "<!--This is a comment-->",
+      "I love JavaScript!",
+    ],
+    answer: "// This is a comment",
+  },
+  {
+    question:
+      "Choose what happens when the follow code snippet runs: console.log('Hello World')",
+    choices: [
+      "'Hello World' is printed to the console",
+      "'Hello World' is stored as a constant variable",
+      "'Hello World' displays on the HTML page",
+      "'Hello World' displays on the CSS page",
+    ],
+    answer: "'Hello World' is printed to the console",
+  },
+];
