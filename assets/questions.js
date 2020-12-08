9ad3124f9d0c3bbe774782e84eb6822fd31075c0
Jessica Blankemeier <jessicablankemeier@gmail.com>
2020-12-07 22:20:42 -0800

    response feedback displaying after question, would like to correct to display during the question

--- assets/questions.js
+++ assets/questions.js
@@ -1,56 +1,44 @@
-const quizQuestions = [
+let questions = [
   {
-    question: "Strings in Javascript are enclosed in what type of punctuation?",
-    choices: [
+    text: "Strings in Javascript are enclosed in what type of punctuation?",
+    answers: [
       "single or double quotes",
       "brackets",
       "curly braces",
       "parenthesis",
     ],
-    answer: "single or double quotes",
+    correctAnswer: "single or double quotes",
   },
   {
-    question:
-      "What will this for loop print to the console: for(let 1=0; i < 3; i++){console.log('Bumfuzzle')}",
-    choices: [
-      "Nothing",
-      "'Bumfuzzle' Once",
-      "'Bumfuzzle' Three Times",
-      "Infinity 'Bumfuzzle'",
-    ],
-    answer: "'Bumfuzzle' Three Times",
-  },
-  {
-    question:
+    text:
       "How could you assign a constant number value 600 as a variable using JavaScript?",
-    choices: [
+    answers: [
       "console.log(600)",
       "var value",
       "let value = 500 - 100",
       "const value = 600",
     ],
-    answer: "const value = 600",
+    correctAnswer: "const value = 600",
   },
   {
-    question:
+    text:
       "What is an example of the syntax for a single line comment in JavaScript",
-    choices: [
+    answers: [
       "console.log(This is a comment)",
       "// This is a comment",
-      "<!--This is a comment-->",
       "I love JavaScript!",
     ],
-    answer: "// This is a comment",
+    correctAnswer: "// This is a comment",
   },
   {
-    question:
+    text:
       "Choose what happens when the follow code snippet runs: console.log('Hello World')",
-    choices: [
+    answers: [
       "'Hello World' is printed to the console",
       "'Hello World' is stored as a constant variable",
       "'Hello World' displays on the HTML page",
       "'Hello World' displays on the CSS page",
     ],
-    answer: "'Hello World' is printed to the console",
+    correctAnswer: "'Hello World' is printed to the console",
   },
 ];
--- assets/quiz.js
+++ assets/quiz.js
@@ -1,45 +1,115 @@
 // Declare game logic variables
 
 let currentQuestionIndex = 0;
+let time = questions.length * 10;
+console.log(questions.length)
+let timerInterval;
+// let score = 0; Saving for future functionality
 
-let choiceEl = document.getElementById("choices");
+
+let answerEl = document.getElementById("answers");
+let endScreenEl = document.getElementById("end-screen")
+let responseEl = document.getElementById("response");
+let homePage = document.getElementById("home-page");
 let questionEl = document.getElementById("questions");
 let startBtn = document.getElementById("start-btn");
 let submitBtn = document.getElementById("submit");
+let timerEl = document.getElementById("time");
 
+//hide home-page, show questions, start timer
 function startQuiz() {
-  let homePage = document.getElementById("home-page");
   homePage.setAttribute("class", "hide");
-
   questionEl.removeAttribute("class");
-
+  timerCountdown();
   getQuestion();
 }
 
+function timerCountdown(){
+  timerInterval = setInterval(function(){
+    timeLeft--;
+    timerEl.textContent = time;
+
+    if(timeLeft <=0){
+      timeLeft = 0;
+      timerEl.textContent = timeLeft;
+      stopQuiz();
+    }
+  }, 1000);
+  
+
+}
+
 function getQuestion() {
-  let currentQuestion = quizQuestions[currentQuestionIndex];
+  let currentQuestion = questions[currentQuestionIndex];
 
   let questionText = document.getElementById("question-text");
-  questionText.textContent = currentQuestion.question;
+  questionText.textContent = currentQuestion.text;
 
-//   // Clear out old question choices
-//   choiceEl.innerHTML = "";
+// Clear any old question answers
+  answerEl.innerHTML = "";
 
-  // Add a button to select each choice
-  currentQuestion.choices.forEach(listChoices);
+  // Dynamically add a button to select each answer 
+  currentQuestion.answers.forEach(listAnswers);
 
-  function listChoices(choice, index) {
-    let choiceBtn = document.createElement("button");
-    choiceBtn.setAttribute("class", "choice");
-    choiceBtn.setAttribute("value", choice)
+  function listAnswers(answer, index) {
+    let answerBtn = document.createElement("button");
+    answerBtn.setAttribute("class", "answer");
+    answerBtn.setAttribute("value", answer);
+ 
+    // increase index so the display starts at 1 and not zero with a )
+    answerBtn.innerHTML += index + 1 + ") " + answer;
 
-    // increase index so it starts at 1 and not zero
-    choiceBtn.innerHTML += index + 1 + ") " + choice;
+    // Make sure the page is listening for a click from the user
+     answerBtn.onclick = answerClick; 
 
     // display on the page
-    choiceEl.appendChild(choiceBtn);
+    answerEl.appendChild(answerBtn);
+  }
 }
+
+function answerClick() {
+  if (this.value !== questions[currentQuestionIndex].correctAnswer) {
+      //penalize time and update timer on page
+     time -=10;
+
+     if (time < 0){
+       time = 0;
+     }
+
+     timerEl.textContent = time;
+
+     responseEl.textContent = "Nope"
+    
+  } else {
+  
+    responseEl.textContent = "Heck Yeah!";
+  }
+
+  //Tell the user how they did for 1 sec
+  responseEl.setAttribute("class", "response");
+  setTimeout(function () {
+    responseEl.setAttribute("class", "response hide");
+  }, 1000);
+
+// Move to the next question as long as there are questions
+currentQuestionIndex++;
+
+if (currentQuestionIndex === questions.length){
+    stopQuiz();
+} else {
+    getQuestion();
 }
+
+}
+
+//Show End Screen and Hide Questions
+function stopQuiz(){
+    clearInterval(time);
+endScreenEl.removeAttribute("class");
+
+questionEl.setAttribute("class", "hide");
+}
+
 
 // Button Actions
 
